9989fbb9ff8d62df80f4de58b487407014dc4227
Hai Le Hong <20485451+hailehong95@users.noreply.github.com>
2022-02-03 17:34:44 +0700

    Add Student Internship

---
+++ student-internship/old-src/week-3/w3-v2/Menu.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+using namespace std;
+
+class Menu
+{
+public:
+	// Các Phương thức hiển thị menu cho người dùng chọn
+	void MenuChinh();
+	void MenuThemSuaXoa();
+	void MenuTimKiemSinhVien();
+	void MenuSapXepSinhVien();
+};
+
---
+++ student-internship/old-src/week-3/w3-v2/Process.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+#include <string>
+#include "SinhVien.h"
+#include <conio.h>
+#include <windows.h>
+using namespace std;
+
+class Process
+{
+public:
+	// Các Phương thức kiểm tra
+	bool KiemTraSinhVien(string sobaodanh, int &vitri);
+	bool KiemTraSoBaoDanh(string sobaodanh);
+	bool KiemTraTongDiem(double tongdiem);
+	bool KiemTraDiemToan(double diemtoan);
+	bool KiemTraDiemHoa(double diemhoa);
+	bool KiemTraDiemLy(double diemly);
+	bool KiemTraHoTen(string hoten);
+
+	// Các Phương thức tìm kiếm
+	void TimKiemSoBaoDanh(string sobaodanh);
+	void TimKiemTongDiem(double tongdiem);
+	void TimKiemHoTen(string hoten);
+	void TimKiemDiemToan(double diemtoan);
+	void TimKiemDiemLy(double diemly);
+	void TimKiemDiemHoa(double diemhoa);
+
+	// Các Phương thức Sắp xếp
+	void SapXepDiemTong();
+	void SapXepSoBaoDanh();
+	void SapXepAnphabe();
+
+	// Các Phương thức khác
+	void ThemSinhvien(int n);
+	void SuaSinhVien(string sobaodanh);
+	void XoaSinhVien(string sobaodanh);
+	void ThongKeTatCa();
+	void XuatSinhVienRaFile(string fileName);
+	void NhapSinhVienTuFile(string fileName);
+
+	DWORD WINAPI threadNhap(LPVOID arg);
+	string Convert(int i);
+	int DemSoFile(string maTruong);
+	void SuaSinhVienv2(string sobaodanh);
+};
